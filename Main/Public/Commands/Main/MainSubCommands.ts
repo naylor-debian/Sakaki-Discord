6abff139dd76edd2ff21f9a110b239c38677b9d0
naylor-debian <95220392+naylor-debian@users.noreply.github.com>
2024-11-13 06:34:18 -0300

    Add files via upload

--- Main/Public/Commands/Main/MainSubCommands.ts
+++ Main/Public/Commands/Main/MainSubCommands.ts
@@ -2,13 +2,13 @@ import { SlashCommandBuilder, EmbedBuilder, } from 'discord.js';
 
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("menu")
-        .setDescription('Command Main !'),
+   data: new SlashCommandBuilder()
+       .setName('subcommands')
+       .setDescription('Options SubCommands'),
 
-    async execute(interaction: any) {
-        const Menu = new EmbedBuilder()
-     .setColor(0x0099FF)
+   async execute (interaction: any) { 
+       const Menu =  new EmbedBuilder()
+    .setColor(0x0099FF)
     .setTitle(`Ola ${interaction.user.globalName}`)
     .setURL('https://naylor-debian.github.io/Sakaki-Services-Web/main.html')
     .setAuthor({ name: `Clique Aqui !`, iconURL: 'https://i.ibb.co/qs37qDm/undefined-image.png', url: 'https://naylor-debian.github.io/Sakaki-Services-Web/main.html' })
@@ -19,16 +19,17 @@ module.exports = {
         size: 1024, })
     )
     .addFields(
-        { name: 'Subcommands', value: '/subcommands' },
-        { name: 'Moderaction', value: '/moderaction' },
+        { name: 'Painel De Osint', value: '/osint' },
+        { name: '\u200B', value: '\u200B' },
         { name: '\u200B', value: '\u200B', inline: true },
         { name: '\u200B', value: '\u200B', inline: true },
     )
     .addFields({ name: '\u200B', value: '\u200B', inline: true })
-    .setImage('https://i.ibb.co/xmXhwQ3/undefined-image.png')
+    .setImage('https://i.ibb.co/WDcwBs4/98916379c2d3e2d5943d7424e63b1598-high.webp')
     .setTimestamp()
     .setFooter({ text: 'All Open Source', iconURL: 'https://i.ibb.co/41CpR5K/901769b2f8a575530b0203c3fdacfd6f-removebg-preview.png' });
 
-        await interaction.reply({ embeds: [Menu] });
-    },
+        await interaction.reply({ embeds: [Menu] });     
+ 
+       },
 };
